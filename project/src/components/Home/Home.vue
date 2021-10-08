ca7e017700baa34866c07b780681779a32808016
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-08 16:37:03 -0300

    Insercao de particoes box de publicacao

--- project/src/components/Home/Home.vue
+++ project/src/components/Home/Home.vue
@@ -9,13 +9,59 @@
         <li style="float: right; margin-right: 100px;">
           <div style="horizontal-align: middle; float: left; display: flex;">
             <a style="color: black;">{{ username }}</a>
-            <img style="border-radius: 50%; height: 30px;" :src="this.avatar" />
+            <img
+              style="border-radius: 50%; height: 30px;"
+              :src="getAvatar(this.avatar)"
+            />
           </div>
         </li>
       </ul>
     </nav>
 
-    <div></div>
+    <!-- Box abaixo menu -->
+    <div style="height: 100vh; justify-content: center; display: flex;">
+      <!-- Box da publicacao -->
+      <div
+        style="border: 2.5px solid #DBDBDB; height: 80vh; width: 150vh; margin-top: 25px; display: flex;"
+      >
+        <!-- Imagem do post -->
+        <div style="height: 100%; width: 60%;">
+          <img style="width: 100%; height: 100%;" :src="this.post['photo']" />
+        </div>
+        <!-- Autor e comentarios -->
+        <div style="height: 100%; width: 40%; float: left;">
+          <!-- Autor -->
+          <div style="border: 1px solid red; width: 100%; height: 15%;">
+            <img
+              style="border-radius: 50%; height: 30px; float: left; "
+              :src="getAvatar(this.post['user']['avatar'])"
+            />
+            <div style="float: left; margin-left: 20px;">
+              <p>{{ this.post["user"]["username"] }}</p>
+              <p>
+                {{ this.post["location"]["city"] }},
+                {{ this.post["location"]["country"] }}
+              </p>
+            </div>
+          </div>
+          <div
+            style="border: 1px solid red; width: 100%; height: 65%; text-align: center;"
+          ></div>
+          <div style="border: 1px solid red; width: 100%; height: 20%;">
+            <p
+              style="font-weight: bold;  margin: 20px; font-family: Arial; font-size: 13px; color: black; "
+            >
+              {{ this.post["comments"].length }} comentários
+            </p>
+            <p
+              style="font-weight: bold; margin: 20px; font-family: Arial; font-size: 13px; color: black;"
+            >
+              {{ this.post["created_at"] }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,13 +77,24 @@ export default {
     return {
       username: "Pesquisando...",
       avatar: "",
+      post: {},
     };
   },
-  methods: {},
+  methods: {
+    getAvatar(avatar) {
+      return avatar == null
+        ? "https://lh3.googleusercontent.com/a/default-user=s40-c"
+        : this.avatar;
+    },
+  },
   async created() {
-    const response = await axios.get("https://taggram.herokuapp.com/me");
-    this.username = response["data"]["username"];
-    this.avatar = response["data"]["avatar"];
+    const userResponse = await axios.get("https://taggram.herokuapp.com/me");
+    this.username = userResponse["data"]["username"];
+    this.avatar = userResponse["data"]["avatar"];
+
+    const url = `https://taggram.herokuapp.com/post?username=${this.username}`;
+    const postResponse = await axios.get(url);
+    this.post = postResponse["data"];
   },
 };
 </script>
