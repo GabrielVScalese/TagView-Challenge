5256d81b70360170b3b1620910c940d0e6a7c3d7
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-11 11:39:05 -0300

    Correcao funcao de tempo

--- project/src/components/Home/Home.vue
+++ project/src/components/Home/Home.vue
@@ -147,24 +147,24 @@ export default {
 
         const diff = Math.abs(date - nowDate);
         const seconds = Math.floor(diff / 1000);
-        const minutes = Math.ceil(diff / (1000 * 60));
-        const hours = Math.floor(diff / (1000 * 60 * 60));
 
         if (seconds >= 60) {
-          if (minutes < 60) return `${minutes}m`;
+          const minutes = Math.floor(seconds / 60);
+
+          if (minutes >= 60) {
+            const hours = Math.floor(minutes / 60);
+
+            if (hours >= 24) {
+              const days = Math.floor(hours / 24);
+
+              if (days >= 7) {
+                const weeks = Math.floor(days / 7);
+
+                return `${weeks}sem`;
+              } else return `${days}d`;
+            } else return `${hours}h`;
+          } else return `${minutes}m`;
         } else return `${seconds}s`;
-
-        if (hours >= 24) {
-          const days = Math.floor(hours / 24);
-
-          if (days >= 7) {
-            const weeks = Math.floor(days / 7);
-
-            return `${weeks}sem`;
-          } else return `${days}d`;
-        }
-
-        return `${hours}h`;
       } else {
         return stringDate;
       }
